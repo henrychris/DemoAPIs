0af91659e770294c2afeb5a197cbc819de11f59d
henrychris <64085210+henrychris@users.noreply.github.com>
2023-07-09 18:58:50 +0100

    setup server on port 3000.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-import * as fs from 'fs'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+import express from 'express';
 
-console.log('Hello world.')
+const PORT = 3000;
+
+dotenv.config();
+const mongoConnString = process.env.DATABASE_URL;
+
+const app = express();
+app.use(express.json());
+
+app.listen(PORT, () => {
+	console.log(`Server started at port ${PORT}.`)
+})
