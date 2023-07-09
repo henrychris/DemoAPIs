936486fa7dc97cd20967221e36bbc063f75ed359
henrychris <64085210+henrychris@users.noreply.github.com>
2023-07-09 19:21:57 +0100

    connect to mongo db

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,29 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose';
 import express from 'express';
 
+// get enviroment variables defined in .env
+dotenv.config();
+
 const PORT = 3000;
 const mongoConnString = process.env.DATABASE_URL;
 
-// get enviroment variables defined in .env
-dotenv.config();
+mongoose.connect(mongoConnString);
+const database = mongoose.connection;
+
+// listen for errors during db connection
+database.on('error', (error) => {
+	console.log(error);
+});
+
+// checks that connection succeeded
+database.once('connected', () => {
+	console.log("Connection Successful.");
+});
+
 
 const app = express();
 app.use(express.json());
 
 app.listen(PORT, () => {
 	console.log(`Server started at port ${PORT}.`)
-})
+});
