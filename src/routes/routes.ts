15e50576edfc90d862e0cdea0c11a155391fca18
henrychris <64085210+henrychris@users.noreply.github.com>
2023-07-09 20:28:57 +0100

    add root and get by serialNumber endpoints

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,17 @@
 import express from 'express';
 const router = express.Router();
 
+// home page
+router.get('/', (req, res) => {
+	res.send('hello world');
+});
+
+// get by serial number
+router.get('/get', (req, res) => {
+	const serialNumber = req.query.serialNumber;
+	res.send(`Henry Ihenacho: ${serialNumber}`);
+});
+
 // balance check
 router.post('/balance-check', (req, res) => {
 	res.send('Agent Balance');
